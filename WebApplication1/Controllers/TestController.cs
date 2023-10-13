1710088d10820db5bdd768e93c72539a4b14620e
Daniel <daniel.ast2007@gmail.com>
2023-10-13 22:34:23 +0300

    Model changed

--- WebApplication1/Controllers/TestController.cs
+++ WebApplication1/Controllers/TestController.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
 using WebApplication1.Controllers.UtilityControllers;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Controllers
 {
 	public class TestController : Controller
 	{
-		private readonly List<Question> questions = new()
-		{
-			new Question(1, "Вопрос 1", 3, 2),
-            		new Question(2, "Вопрос 2", 3, 4),
-          		new Question(3, "Вопрос 3", 5, 4),
-			new Question(4, "Вопрос 4", -1, 5),
-			new Question(5, "Вопрос 5", -1, -1)
-        };
-
 		public IActionResult Index(int id = 1)
 		{
 			if (id > 0)
---
+++ WebApplication1/Controllers/UtilityControllers/QuestionList.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.UtilityControllers
+{
+	public class QuestionList
+	{
+		public static List<Question> questions;
+
+		public static void Init()
+		{
+			questions = new List<Question>();
+		}
+	}
+}
