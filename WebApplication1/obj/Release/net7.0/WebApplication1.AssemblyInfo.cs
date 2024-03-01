ca66166c027798890476970b99bb34c56431aa06
Daniel <daniel.ast2007@gmail.com>
2024-03-01 23:01:12 +0300

    searchstring fixed

--- WebApplication1/obj/Release/net7.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net7.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e49dc6a928c4e1675e6c0694e33503ad26bca889")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
